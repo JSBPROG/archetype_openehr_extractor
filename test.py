57afe4bd14699dc8baf423e8004371eb66c2cbb2
Jorge SB <167673003+JSBPROG@users.noreply.github.com>
2025-07-19 17:59:02 +0200

    Update test.py

--- test.py
+++ test.py
@@ -3,12 +3,12 @@ import json
 
 
 if __name__ == "__main__":
-    extractor = Extractor(output_path="./output")  # puedes cambiar la ruta si quieres
+    extractor = Extractor(output_path="./output")  
     extractor.load_doc_list()
     extractor.extract_all()
-    extractor.save_json_list()  # guarda data_from_xml.json
+    extractor.save_json_list()  
 
-    extractor.generate_simplified_json(save=True)  # guarda simplified_data.json
+    extractor.generate_simplified_json(save=True)  
 
 
 
